0449e577d13c0ec16356b73850ce23f2cac48f81
akeszei <alex.keszei@gmail.com>
2025-01-15 10:14:36 -0500

    Update recast_mrc_f16_to_f32.py

--- recast_mrc_f16_to_f32.py
+++ recast_mrc_f16_to_f32.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+
+def usage():
+    print("===================================================================================================")
+    print(" Quickly convert .MRC mode 12 (float16) image to .MRC mode 2 (float32) for compability with ")
+    print(" programs that cannot read float16 images (i.e. CTFFIND4). ")
+    print(" Usage:")
+    print("    $ recast_mrc_f16_to_f32.py  input.mrc  desired/output/path ")
+    print(" Example for converting an entire directory:")
+    print("    $ for m in *.mrc; do recast_mrc_f16_to_f32.py $m f32/; done")
+    # print(" -----------------------------------------------------------------------------------------------")
+    # print(" Options (defaults in brackets): ")
+    # print("        --jpg (4) : also save a (binned) .jpg image of the .MRC file")
+    # print("          --j (4) : batch mode is optionally multithreaded across given # of cores")
+    print("===================================================================================================")
+    sys.exit()
+
 def get_mrc_data(mrc_file):
     
     with mrcfile.open(mrc_file, permissive = True, mode = 'r') as mrc:
@@ -27,6 +43,17 @@ if __name__ == "__main__":
     import mrcfile 
     import numpy as np
     import sys, os
+
+    cmdline = sys.argv 
+    min_args = 2
+
+    if len(cmdline) - 1 < min_args:
+        print(" Not enough arguments")
+        usage()
+
+    for entry in cmdline:
+        if entry in ['-h', '-help', '--h', '--help']:
+            usage()
 
     print("==================================================================")
 
